241e6b41bfc793c803a380d5a221174ef1902d7e
John Keefe <john@johnkeefe.net>
2018-05-09 17:41:54 -0400

    added file writing

--- chinanonfiction/index.js
+++ chinanonfiction/index.js
@@ -1,5 +1,9 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+
+// run using "node index.js"
+// makes a file called output.tsv
 
 var url = "http://www.chinanonfiction.com";
 
@@ -11,6 +15,8 @@ request(url, function(error, response, body){
     
     var story;
     
+    var output = "title\tcontent\n";
+    
     console.log("title\tcontent");
     
     $('article').each(function(i, elem){
@@ -21,10 +27,17 @@ request(url, function(error, response, body){
         var content = story('.entry-summary').text().trim();
         console.log(title + "\t" + content);
         
+        output += title + "\t" + content + "\n";
+        
     });
     
-    // var random = Math.floor(Math.random() * set.length);
-        
-    // console.log(set[random]);
+    fs.writeFile("output.tsv", output, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+});
+    
     
 });
